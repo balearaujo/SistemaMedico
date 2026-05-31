9dc688814f41a23eb56b85674750db5d09868def
balearaujo <vale.araujomartinez@gmail.com>
2026-05-31 00:04:32 -0600

    Intento de diseño de login, imagen agregada, se debe trabajar el fondo

--- src/login/Bienvenida.java
+++ src/login/Bienvenida.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -12,6 +13,7 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Canvas;
+import javax.swing.SwingConstants;
 
 public class Bienvenida extends JFrame {
 
@@ -48,7 +50,9 @@ public class Bienvenida extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Iniciar Sesión");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(0, 87, 174));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				VentanaLogin login=new VentanaLogin();
@@ -60,7 +64,9 @@ public class Bienvenida extends JFrame {
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Registrarse");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnNewButton_1.setBackground(new Color(0, 87, 174));
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				VentanaSignin signin=new VentanaSignin();
@@ -75,8 +81,38 @@ public class Bienvenida extends JFrame {
 		lblNewLabel.setForeground(new Color(0, 64, 128));
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 48));
-		lblNewLabel.setBounds(259, 82, 199, 69);
+		lblNewLabel.setBounds(263, 78, 199, 69);
 		contentPane.add(lblNewLabel);
+		
+		JLabel Imagen = new JLabel("");
+		Imagen.setBounds(162, 31, 99, 102);
+		Imagen.setOpaque(false);
+		try {
+			java.net.URL imURL= getClass().getResource("/Imagenes/Logo.png");
+			if (imURL != null) {
+				ImageIcon icon=new ImageIcon(imURL);
+				
+				java.awt.Image imgEscalada = icon.getImage().getScaledInstance(
+			            Imagen.getWidth(), 
+			            Imagen.getHeight(), 
+			            java.awt.Image.SCALE_SMOOTH 
+			        );
+				ImageIcon icoEscalado = new ImageIcon(imgEscalada);
+		        Imagen.setIcon(icoEscalado);
+			} else {
+				System.out.printf("Ruta no encontrada");
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Error al cargar la imagen: " + e.getMessage());
+		}
+		contentPane.add(Imagen);
+		
+		JLabel lblNewLabel_1 = new JLabel("Sistema de Gestion: Consultorio dental");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1.setBounds(243, 135, 226, 12);
+		
+		contentPane.add(lblNewLabel_1);
 
 	}
 }
